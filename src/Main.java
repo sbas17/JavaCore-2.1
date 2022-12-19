feca4d3e5269be681e2cfa2848faed5df25f763d
Anastasia Belogolovtseva <somova_as@mail.ru>
2022-12-19 22:36:46 +0300

    add files

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> newList = new ArrayList<>();
+        for (int x : intList) {
+            if (x > 0) {
+                if (x % 2 == 0) {
+                    newList.add(x);
+                }
+            }
+        }
+        Collections.sort(newList);
+        System.out.println(newList);
+    }
+}
